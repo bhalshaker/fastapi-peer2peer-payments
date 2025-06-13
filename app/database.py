30ee6ac748b3cb20c14e7d3e017c31858b107df8
Ebrahim Alshaker <alshaker@proton.me>
2025-06-13 21:13:10 +0300

    Fix seed.py

--- app/database.py
+++ app/database.py
@@ -1,20 +1,23 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import sessionmaker
 from config import config
 
 #Generate database URL from configuration
 db_url=f"postgresql+asyncpg://{config.db_user}:{config.db_password}@{config.db_host}:{config.db_port}/{config.db_name}"
 # Create an asynchronous engine and configurre pool size
 engine = create_async_engine(db_url, pool_size=config.db_pool_size, max_overflow=config.db_max_overflow)  # Adjust pool size as needed
-# Create an asynchronous session factory
+# Create an asynchronous session factory using async_sessionmaker
 SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Yields a database session as a dependency that can be used in FastAPI routes.
+    
+    Usage in FastAPI:
+        @app.get("/users")
+        async def get_users(db: AsyncSession = Depends(get_db_session)):
+            ...
+    """
     async with SessionLocal() as session:
         yield session
-        
-async def recreate_db(Base):
-    # Drop and recreate tables to ensure a clean slate
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
--- app/seed.py
+++ app/seed.py
@@ -1,10 +1,12 @@
 import database
 from models import Base
-from logging import getLogger
+import logging
 from schema import CreateUserSchema
 from controllers import CreateUserController
 from config import config
 import asyncio
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("Seed Database")
 
 async def recreate_db(engine,Base):
     # Drop and recreate tables to ensure a clean slate
@@ -12,20 +14,25 @@ async def recreate_db(engine,Base):
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
+async def create_admin_user():
+# Create an admin user based on the configuration settings
+   admin_user=CreateUserSchema(username=config.admin_username,
+                               email=config.admin_email,
+                               plain_password=config.admin_password,
+                               first_name=config.admin_first_name,
+                               last_name=config.admin_last_name)
+   # Create session and call the controller to create the admin user
+   async with database.SessionLocal() as session:
+        await CreateUserController(admin_user,session, is_admin=True)
+
 async def main():
-    logger= getLogger(__name__)
     logger.info("Recreating database... 🫕")
     # Recreate the database schema
     try:
       await recreate_db(database.engine, Base)
       logger.info("Database recreated successfully ... 🍜")
       logger.info("Creating admin user based on configuration... 🧑‍💻")
-      admin_user=CreateUserSchema(user_name=config.admin_username,
-                               email=config.admin_email,
-                               plain_password=config.admin_password,
-                               first_name=config.admin_first_name,
-                               last_name=config.admin_last_name)
-      await CreateUserController(admin_user,database.get_db(), is_admin=True)
+      await create_admin_user()
       logger.info("Admin user created successfully based on your configuration settings... 🪪")
       logger.info("Database seeding completed successfully, You can start using the application now... 🎉")
     except Exception as e:
